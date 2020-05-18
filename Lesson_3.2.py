f46c5e2d39f4eeb28d84ef5df30a5819623dc70b
Sergey0381 <62644920+Sergey0381@users.noreply.github.com>
2020-05-18 14:09:26 +0300

    Added tasks of the third lesson
    except for the last job

---
+++ Lesson_3.2.py
@@ -0,0 +1,15 @@
+# Task  2
+
+def person(surname, name, age, year, city, email, phone):
+    return
+
+
+surname = input('Вевдите свою фамилию:')
+name = input('Вевдите своё имя:')
+age = int(input('Введите свой возраст:'))
+year = int(input('Введите свой год рождения:'))
+city = input('Введите город по прописке :')
+email = input('Ваша электронная почта:')
+phone = input('Введите свой контактный телефон:')
+print("Фамилия:", surname, ';''Имя:', name, ';''возраст:', age, 'лет'';', 'Город:', city, ';''Электронный адрес:',
+      email, ';''Контактный телефон:', phone)
